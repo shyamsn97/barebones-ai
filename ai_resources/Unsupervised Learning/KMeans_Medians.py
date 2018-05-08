eb90f8ea214ebef1d318d6beecede55bbeef5712
Shyam <shyam.sudhakaran97@gmail.com>
2018-05-07 19:22:18 -0700

    Kmeandians

--- ai_resources/Unsupervised Learning/KMeans_Medians.py
+++ ai_resources/Unsupervised Learning/KMeans_Medians.py
@@ -2,10 +2,10 @@ import numpy as np
 import sys
 sys.path.append('../tools')
 import tools
-class Kmeans():
-    
+
+class KMeans_Medians():
     """
-    Vanilla Kmeans:
+    Kmeans / Kmedians:
     Parameters:
         X: numpy array() data matrix
         center_assignments: dictionary of numpy arrays of indices for data points
@@ -16,21 +16,29 @@ class Kmeans():
         self.center_assignments = {}
         self.centers = []
         
-    def calculate_centers(self):
-        self.centers = np.array(list(map(lambda x : np.mean(X[x,:],axis=0),self.center_assignments.values())))
-        
-    def predict(self,k,seed,exit=0.001):
-        
+    def calculate_centers(self,dist=1):
+        if dist == 1:
+            self.centers = np.array(list(map(lambda x : np.mean(X[x,:],axis=0),self.center_assignments.values())))
+        elif dist == 2:
+            self.centers = np.array(list(map(lambda x : np.median(X[x,:],axis=0),self.center_assignments.values())))
+
+
+    def predict(self,k,seed,exit=0.01,dist_type = "mean",maxiterations=100):
+        self.center_assignments = {}
+        self.centers = []
         X = self.X
         self.centers = np.zeros(shape=(k,X.shape[1]))
-        print(self.centers.shape)
         for i in range(k):
             for j in range(X.shape[1]):
                 minx = np.min(X[:,j])
                 maxx = np.max(X[:,j])
                 self.centers[i,j] = np.random.uniform(minx,maxx)
         oldcenters = self.centers + 4
+        count = 0
         while np.all(np.linalg.norm((self.centers - oldcenters),2) >= exit):
+            count += 1
+            if count == maxiterations:
+                break
             oldcenters = self.centers
             for i in range(k):
                 self.center_assignments[i] = []
@@ -39,7 +47,11 @@ class Kmeans():
                 sample = ones.dot(X[i,:].reshape(1,X.shape[1]))
                 closest = np.argmin(np.linalg.norm(oldcenters - sample,2,axis=1))
                 self.center_assignments[closest].append(i)
-                self.calculate_centers()
+                if dist_type == "mean":
+                    self.calculate_centers(1)
+                elif dist_type == "median":
+                    self.calculate_centers(2)
+                    
         end = np.zeros(X.shape[0])
         for i in range(k):
             end[self.center_assignments[i]] = i
